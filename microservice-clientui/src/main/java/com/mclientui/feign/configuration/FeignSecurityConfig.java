ae70139495ce3db3bad1d8447705c0394d607fb8
chat_roux <tdalgalian@hotmail.com>
2018-10-12 20:23:16 +0200

    Framework : 'Spring-cloud-starter-security' -- Mise en place (en cours)

---
+++ microservice-clientui/src/main/java/com/mclientui/feign/configuration/FeignSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.mclientui.feign.configuration;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import feign.auth.BasicAuthRequestInterceptor;
+
+
+/**
+ * <b>COMPOSANT QUI IMPLEMENTE LES FONCTIONNALITES CI-DESSOUS:</b><br/>
+ *    ->GESTION DES INTERCEPTEURS SPECIFIES CI-DESSOUS.<br/>
+ *    ->TYPE D'INTERCEPTEURS : INTERCEPTEURS DE REQUETES D'AUTHENTIFICATION 'BasicAuthRequestInterceptor'
+ */
+@Configuration
+public class FeignSecurityConfig {
+	
+	
+	/**
+	 * <b>OBJET QUI POSSEDE LES FONCTIONNALITES SUIVANTES : </b> <br/>
+	 * <br/>
+	 * Les fonctionnalites d'ecriture de messages de log dans la console.
+	 */
+	private static final Logger LOGGER = LoggerFactory.getLogger(FeignSecurityConfig.class);
+	
+	
+	@Bean
+    public BasicAuthRequestInterceptor mBasicAuthRequestInterceptor(){
+		
+		LOGGER.info("CLASS : FeignSecurityConfig -- METHOD : mBasicAuthRequestInterceptor -- BEGIN");
+		BasicAuthRequestInterceptor basicAuthRequestInterceptor = new BasicAuthRequestInterceptor("utilisateur", "mdp");
+		LOGGER.info("CLASS : FeignSecurityConfig -- METHOD : mBasicAuthRequestInterceptor -- END");
+        return  basicAuthRequestInterceptor;
+    }
+}
