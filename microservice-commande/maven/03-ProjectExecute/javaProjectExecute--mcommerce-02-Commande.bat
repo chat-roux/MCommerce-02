846185b47dec40934292621da3c37c8149fa5932
chat_roux <tdalgalian@hotmail.com>
2018-10-15 02:47:48 +0200

    Application : 'microservice-commande' -- Fichier batch d'exécution : 'javaProjectExecute--mcommerce-02-Commande.bat' -- Commande d'exécution : Modifié le fichier de sortie des logs

--- microservice-commande/maven/03-ProjectExecute/javaProjectExecute--mcommerce-02-Commande.bat
+++ microservice-commande/maven/03-ProjectExecute/javaProjectExecute--mcommerce-02-Commande.bat
@@ -7,10 +7,10 @@ java -verbose ^
  -Dserver.port=9020 ^
  -jar ^
  ./target/mcommande.jar ^
- > .\logExecute\run--mcommerce-02-Commande--Port9020--%date:~6,4%.%date:~3,2%.%date:~0,2%--0%time:~1,1%.%time:~3,2%.%time:~6,2%.log
+ > .\logExecute\execute--mcommerce-02-Commande--Port9020--%date:~6,4%.%date:~3,2%.%date:~0,2%--0%time:~1,1%.%time:~3,2%.%time:~6,2%.log
  
 java -verbose ^
  -Dserver.port=9020 ^
  -jar ^
  ./target/mcommande.jar ^
- > .\logExecute\run--mcommerce-02-Commande--Port9020--%date:~6,4%.%date:~3,2%.%date:~0,2%--%time:~0,2%.%time:~3,2%.%time:~6,2%.log
+ > .\logExecute\execute--mcommerce-02-Commande--Port9020--%date:~6,4%.%date:~3,2%.%date:~0,2%--%time:~0,2%.%time:~3,2%.%time:~6,2%.log
